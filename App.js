8a0075677dc9c02b06b0ab51974d566b526855fc
Elvis Agbesi <elvisagbesi@gmail.com>
2021-03-08 19:55:11 +0000

    feat: :construction: Navigation/ Action bar built

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-
+function RoundedButton({ children, onPress = () => { } }) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.roundedButton}>
+      {children}
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between',paddingHorizontal: 50 }}>
 
-        <View>
-          <Text style={{fontSize: 30, fontWeight: '600'}}>Notes</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 30, marginTop: 20 }}>
+
+        <View style={{ alignSelf: 'center' }}>
+          <Text style={{ fontSize: 30, fontWeight: '600',color:'white' }}>Notes</Text>
         </View>
 
         <View>
-          <Feather name="search" size={24} color="black" />
+          <RoundedButton>
+            <Feather name="search" size={24} color="white" />
+          </RoundedButton>
         </View>
 
       </View>
+
 
     </SafeAreaView>
   );
@@ -26,6 +36,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: '#252525',
+    flex: 1
   },
+  roundedButton: {
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#3B3B3B"
+  }
 });
