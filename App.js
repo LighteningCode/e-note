5e145c085101041d6292a63f164d26e1b9faea81
Elvis Agbesi <elvisagbesi@gmail.com>
2021-03-09 23:34:59 +0000

    fix: :bug: Fixing a layout bug

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 
@@ -20,7 +20,7 @@ const COLORS = {
 
 function ActionBar() {
   return (
-    <TouchableOpacity style={{ width: 60, backgroundColor: "black", height: 60, borderRadius: 20, justifyContent: 'center', position: 'absolute', bottom: 0, right: 0, margin: 20, shadowColor: 'black',shadowRadius: 10,shadowOpacity:0.5, shadowOffset: { width: 0, height: 5 } }}>
+    <TouchableOpacity style={{ width: 60, backgroundColor: "black", height: 60, borderRadius: 20, justifyContent: 'center', position: 'absolute', bottom: 0, right: 0, margin: 20, shadowColor: 'black', shadowRadius: 10, shadowOpacity: 0.5, shadowOffset: { width: 0, height: 5 } }}>
       <Feather name="plus" color="white" size={30} style={{ alignSelf: 'center' }} />
     </TouchableOpacity>
   )
@@ -46,7 +46,7 @@ function NoteCardView({ title, date = "No date", backgroundColor = "#616161", ty
     comp_height = comp_width;
   } else if (type === "wide") {
     comp_height = MAX_ALLOWED_WIDTH / 2
-    comp_width = MAX_ALLOWED_WIDTH + (CARD_MARGIN*2)
+    comp_width = MAX_ALLOWED_WIDTH + (CARD_MARGIN * 2)
     datePosition = 'right'
   } else if (type = "long") {
 
@@ -55,9 +55,9 @@ function NoteCardView({ title, date = "No date", backgroundColor = "#616161", ty
   return (
     <View style={{ backgroundColor: backgroundColor, width: comp_width, height: comp_height, borderRadius: 15, padding: 12, margin: CARD_MARGIN, justifyContent: 'space-between' }}>
       <View style={{ flex: 10 }}>
-        <Text style={{ fontSize: 24, fontWeight: '500' }}>{title}</Text>
+        <Text ellipsizeMode='tail' lineBreakMode='tail' numberOfLines={4} style={{ fontSize: 24, fontWeight: '500' }}>{title}</Text>
       </View>
-      <Text style={{ fontSize: 16, fontWeight: '300', flex: 2, textAlign:datePosition  }}>{date}</Text>
+      <Text style={{ fontSize: 16, fontWeight: '300', flex: 2, textAlign: datePosition }}>{date}</Text>
     </View>
   )
 
@@ -68,7 +68,7 @@ export default function App() {
     <SafeAreaView style={{ flex: 1, backgroundColor: '#252525' }}>
       <View style={styles.container}>
 
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20,position:'absolute',zIndex:500, width:device_width, paddingHorizontal: 40}}>
 
           <View style={{ alignSelf: 'center' }}>
             <Text style={{ fontSize: 30, fontWeight: '600', color: 'white' }}>Notes</Text>
@@ -82,11 +82,17 @@ export default function App() {
 
         </View>
 
-        <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-          <NoteCardView backgroundColor={COLORS.red} title={"How to make your personal brand stand out"} date={"May 21, 2020"} />
-          <NoteCardView backgroundColor={COLORS.pink} title={"School rep and the story"} />
-          <NoteCardView backgroundColor={COLORS.orange} title={"Scenery and places to work in 2020"} type="wide" />
-        </View>
+        <ScrollView style={{height:device_height, paddingTop: 70, flex: 1}}>
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between',height: device_height,flex: 1 }}>
+            <NoteCardView backgroundColor={COLORS.red} title={"How to make your personal brand stand out"} date={"May 21, 2020"} />
+            <NoteCardView backgroundColor={COLORS.pink} title={"School rep and the story"} />
+            <NoteCardView backgroundColor={COLORS.orange} title={"Scenery and places to work in 2020"} type="wide" />
+            <NoteCardView backgroundColor={COLORS.blue} title={"Being a Christian in 2020"} type="wide" />
+            <NoteCardView backgroundColor={COLORS.pink} title={"What independence means to mesfafsasfhashfshafhs sahshasfh"} />
+            <NoteCardView backgroundColor={COLORS.blue} title={"Being a Christian in 2020"} type="wide" />
+            <NoteCardView backgroundColor={COLORS.pink} title={"What independence means to mesfafsasfhashfshafhs sahshasfh"} />
+          </View>
+        </ScrollView>
 
       </View>
 
@@ -99,7 +105,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#252525',
     paddingHorizontal: MAIN_PADDING,
-    flex: 1
+    height: device_height
   },
   roundedButton: {
     borderRadius: 10,
