23cf7e41d888a8a5c6c1691fed0d0cdef4610065
Elvis Agbesi <elvisagbesi@gmail.com>
2021-03-10 23:30:53 +0000

    feat: :construction: Take note screen craeted

---
+++ screens/TakeNote.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+
+
+function TakeNote(){
+    return(
+        <View>
+            <Text>Take note screen</Text>
+        </View>
+    )
+}
+
+
+export default TakeNote;
